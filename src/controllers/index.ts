96b3e09a59f4c7b3cb56219fc541f3a0bd50081a
GuauDog <73758776+GuauDog@users.noreply.github.com>
2022-10-21 20:32:59 -0500

    controllers Vuelos y Rutas

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,5 @@
 export * from './ping.controller';
 export * from './usuario.controller';
 export * from './aeropuerto.controller';
+export * from './vuelos.controller';
+export * from './rutas.controller';
